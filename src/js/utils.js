78e72953177d5b794c326aa98ae5d2fa744fbc25
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-09-08 19:10:42 -0600

    Blog filtering and customizing the home page

--- src/js/utils.js
+++ src/js/utils.js
@@ -15,3 +15,36 @@ export function formatDate(date){
         timeZone: "UTC",
     });
 }
+
+
+export function formatBlogPosts(posts, {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined
+
+} = {}){
+    const filteredPosts = posts.reduce((acc, post) => {
+        const {date, draft} = post.frontmatter;
+        // Filter out Drafts if true
+        if (filterOutDrafts && draft) return acc;
+        if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+        // Add the post to the acumulator
+        acc.push(post);
+        return acc;
+    }, [])
+
+    // Sort by Date or randomize
+    if (sortByDate){
+        filteredPosts.sort((a, b) => 
+            new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+    } else{
+        filteredPosts.sort(() => Math.random() - 0.5);
+    }
+
+    // Limit the posts
+    if (typeof limit == "number"){
+        return filteredPosts.slice(0, limit);
+    }
+    return filteredPosts;
+}
